249e5d99a28d676fb6bd348c97525116a3dbc61b
Renaldi <renaldisimamora67@gmail.com>
2023-09-16 00:45:29 +0700

    ✨ commit and push new feature

--- SimpleServiceDeskBuilding/Services/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleServiceDeskBuilding.Services
-{
-    public interface IUnitOfWork
-    {
-        public IUserService UserService { get; }
-        public ITicketBuildingService TicketBuildingService { get; }
-        public IActivityTicketBuildingService ActivityTicketBuildingService { get; }
-    }
-}
